1a33f7a9ad1f06b1124db37623abcab09fe2ef21
DereckAn <dereckangeles15071998@gmail.com>
2025-11-19 21:23:50 -0700

    fixing macos release and linux ci

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,6 +12,8 @@ use commands::AppState;
 use std::sync::Mutex;
 #[cfg(target_os = "macos")]
 use tauri::image::Image;
+#[cfg(target_os = "macos")]
+use tauri::path::BaseDirectory;
 use tauri::menu::{Menu, MenuItem};
 use tauri::tray::TrayIconBuilder;
 use tauri::{Emitter, Manager};
